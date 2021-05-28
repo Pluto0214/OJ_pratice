5dd9854c806567b74fd439155a828881e29cba7c
张文昊 <815636627@qq.com>
2021-05-28 20:33:12 +0800

    OJ

---
+++ OJ_210528/test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+//二叉搜索树与双向链表
+class Solution {
+public:
+	Node* treeToDoublyList(Node* root) {
+		if (root == nullptr) return nullptr;
+		dfs(root);
+		head->left = pre;
+		pre->right = head;
+		return head;
+	}
+private:
+	Node *pre, *head;
+	void dfs(Node* cur) {
+		if (cur == nullptr) return;
+		dfs(cur->left);
+		if (pre != nullptr) pre->right = cur;
+		else head = cur;
+		cur->left = pre;
+		pre = cur;
+		dfs(cur->right);
+	}
+};
